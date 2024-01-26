bc5668e744f2c979ba6d927f51fcb29ffa76d948
Michael Little <michael.little@xero.com>
2024-01-26 13:40:12 +1300

    Can render multiple cards with name and description

--- render.js
+++ render.js
@@ -1,3 +1,58 @@
+const elementWithClasses = (elementName, cssClasses) => {
+    const newElement = document.createElement(elementName);
+
+    if(Array.isArray(cssClasses)) {
+        newElement.classList.add(...cssClasses);
+
+    } else {
+        newElement.className = cssClasses;
+    }
+
+    return newElement;
+}
+
+const p = (cssClasses, text) => {
+    const newElement = elementWithClasses('p', cssClasses)
+
+    newElement.innerHTML = text;
+    
+    return newElement;
+} 
+
+const h = (size) => {
+    return (cssClasses, text) => {
+        const newElement = elementWithClasses(`h${size}`, cssClasses)
+
+        newElement.innerHTML = text;
+        
+        return newElement;
+    }
+}
+
+const h1 = h(1);
+const h2 = h(2);
+const h3 = h(3);
+const h4 = h(4);
+
+const div = (cssClasses, children = []) => {
+    const newElement = elementWithClasses('div', cssClasses);
+   
+    newElement.append(...children);
+    
+    return newElement;
+}
+
+const spellCard = (spell) => 
+    div('spellCard', [
+        h1('name', spell.name),
+        ...spell.description.split('\n').map(l => p('description', l))
+    ]);
+
+const spellCardList = (spellCards) => div('spellCards', spellCards);
+
+
 const render = (rootElement, data) => {
-    rootElement.innerHTML = JSON.stringify(data);
+    rootElement.append(
+        spellCardList(data.spells.map(s => spellCard(s)))
+    );
 }
